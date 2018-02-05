72c5745b7acfe23a7ccea1eb1686f77254d4491c
Estudiante <Estudiante@10.203.0.203>
2018-02-05 08:20:24 -0500

    farid clase 1

---
+++ src/HolaMundo.java
@@ -0,0 +1,19 @@
+
+import static java.time.Clock.system;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class HolaMundo {
+ public static void main( String []arg){
+ System.out.println("Hola mundo");    
+ }  
+}
+
